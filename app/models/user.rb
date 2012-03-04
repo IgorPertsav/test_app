b6dd24d0b6881432186d4449359aa8974eb6551b
Igor Pertsav <pertsavigorstray@gmail.com>
2012-03-04 19:51:06 +0300

    User model with show + pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,39 @@ class User < ActiveRecord::Base
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
+  attr_accessor :password, :password_confirm
+
+  validates :password, :confirmation => true, :presence => true, :length => {:within => 6..20}
   validates :name, :presence => true, :uniqueness => {:case_sensitive => false}, :length =>{:maximum => 20}
   validates :email, :presence => true, :uniqueness => {:case_sensitive => false}, :format => {:with => email_regex}
+
+  before_save :encrypt_password
+
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil  if user.nil?
+    return user if user.has_password?(submitted_password)
+  end
+
+  private
+  def encrypt_password
+    self.salt = make_salt if new_record?
+    self.encrypted_password = encrypt(password)
+  end
+
+  def encrypt(string)
+    secure_hash("#{salt}--#{string}")
+  end
+
+  def make_salt
+    secure_hash("#{Time.now.utc}--#{password}")
+  end
+
+  def secure_hash(string)
+    Digest::SHA2.hexdigest(string)
+  end
 end
