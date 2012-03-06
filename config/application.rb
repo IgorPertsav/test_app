4c88687cf1c64e65b085d52b42c9fdf9a3c1f2ba
Igor Pertsav <pertsavigorstray@gmail.com>
2012-03-06 15:45:32 +0300

    end

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module TestApp
 
     # Custom directories with classes and modules you want to be autoloadable.
     #config.autoload_paths += %W(#{config.root}/extras)
-    config.autoload_paths += %W["#{config.root}/**/"]
+    #config.autoload_paths += %W["#{config.root}/**/"]
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
